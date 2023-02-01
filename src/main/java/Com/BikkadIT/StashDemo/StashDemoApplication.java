22eaff1c4a3bf0e737e9f1898caefb1731857a83
Dhanshri210 <123482151+Dhanshri210@users.noreply.github.com>
2023-02-01 23:38:23 +0530

    Changes By Dhanshri

---
+++ src/main/java/Com/BikkadIT/StashDemo/StashDemoApplication.java
@@ -0,0 +1,13 @@
+package Com.BikkadIT.StashDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StashDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StashDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Com/BikkadIT/StashDemo/StashDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.BikkadIT.StashDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StashDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
